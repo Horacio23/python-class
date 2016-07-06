4dedb26ba0cf05304905105999bd5df60ebcff37
Horacio Delgado <horacio.delgado23@gmail.com>
2016-07-06 11:59:49 -0400

    changed the module to accept a URL and fetch words from it

--- words.py
+++ words.py
@@ -1,7 +1,8 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
@@ -13,9 +14,9 @@ def print_items(items):
     for item in items:
         print(item)
 
-def main():
-    words = fetch_words()
+def main(url):
+    words = fetch_words(url)
     print_items(words)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
