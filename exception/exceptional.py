5ac8501f6705e771f03105291c15ea2c8d9deb22
horacio23 <horacio.delgado23@gmail.com>
2016-07-10 16:03:51 -0400

    used the raise keyword to pass the error up the hierarchy

--- exception/exceptional.py
+++ exception/exceptional.py
@@ -1,9 +1,14 @@
 '''A module for demonstrating exceptions.'''
 
+import sys
+
 def convert(s):
     '''Convert to an integer.'''
     try:
         return int(s)
         print("Conversion succeeded! x =",x)
-    except (ValueError, TypeError):
-        return -1
+    except (ValueError, TypeError) as e:
+        print("Conversion error: {}"\
+                .format(str(e)),
+                file=sys.stderr)
+        raise
