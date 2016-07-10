47f20458b386c4b4c32b43c6d3b7bd7eb87769e8
horacio23 <horacio.delgado23@gmail.com>
2016-07-10 15:52:01 -0400

    Added 2 more examples

---
+++ strings/string.py
@@ -0,0 +1,16 @@
+def stringJoin():
+    s = ';'.join(['pal','ineed','haro'])
+    print(s)
+
+def concatinate():
+    s = ['pal','ind','rome']
+    res = ''.join(s)
+    print(res)
+
+
+def main():
+    stringJoin()
+    concatinate()
+
+if __name__=='__main__':
+    main()
