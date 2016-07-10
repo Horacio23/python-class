5c701b02c87dbd48eb521058e440587dab094c15
horacio23 <horacio.delgado23@gmail.com>
2016-07-10 17:42:03 -0400

    replaced the print stament to show custom message

--- exception/roots.py
+++ exception/roots.py
@@ -1,3 +1,4 @@
+import sys
 def sqrt(x):
     '''Compute square roots using the method of Heron of Alexandria.
 
@@ -7,6 +8,8 @@ def sqrt(x):
     Returns:
         The square root of x.
     '''
+    if x<0:
+        raise ValueError("Cannot compute square root of negative number {}".format(x))
     guess = x
     i = 0
     while guess * guess != x and i < 20:
@@ -19,8 +22,8 @@ def main():
         print(sqrt(9))
         print(sqrt(2))
         print(sqrt(-1))
-    except ZeroDivisionError:
-        print("Cannot compute the square root of a negative number.")
+    except ValueError as e:
+        print(e, file=sys.stderr)
     print("Program execution continues normally here")
 
 if __name__ == '__main__':
