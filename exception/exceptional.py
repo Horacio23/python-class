b23d0a31eae6b722d5a0cd905d9d46e0215108e2
horacio23 <horacio.delgado23@gmail.com>
2016-07-10 15:52:58 -0400

    Eliminated code duplication by using a touple in the exceptions

--- exception/exceptional.py
+++ exception/exceptional.py
@@ -2,13 +2,10 @@
 
 def convert(s):
     '''Convert to an integer.'''
+    x=-1
     try:
         x = int(s)
         print("Conversion succeeded! x =",x)
-    except ValueError:
+    except (ValueError, TypeError):
         print("Conversion failed!")
-        x=-1
-    except TypeError:
-        print("Conversion failed!")
-        x=-1
     return x
